68bfbd3e3f05215ae6f71e5a1e034fa42ddbd5ff
shahadat13593 <shahadat15-13593@diu.edu.bd>
2023-01-02 19:24:39 +0600

    01

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -41,39 +41,105 @@
 // ! Basic Operator
 
 // ! Arithmetic Operator
-const currentYear = 2023;
-const ageShahadat = currentYear - 2001;
-const agePinky = currentYear - 2002;
-console.log(ageShahadat, agePinky);
+// const currentYear = 2023;
+// const ageShahadat = currentYear - 2001;
+// const agePinky = currentYear - 2002;
+// console.log(ageShahadat, agePinky);
 
-// ? 2 ** 3 means 2 to the power 3
-console.log(ageShahadat * 2, ageShahadat / 10, 2 ** 3);
+// // ? 2 ** 3 means 2 to the power 3
+// console.log(ageShahadat * 2, ageShahadat / 10, 2 ** 3);
 
-const firstName = "Shahadat";
-const lastName = "Hossain";
-console.log(firstName + " " + lastName);
+// const firstName = "Shahadat";
+// const lastName = "Hossain";
+// console.log(firstName + " " + lastName);
 
 // ! Assignment Operator
-let x = 10 + 5;
-console.log(x);
+// let x = 10 + 5;
+// console.log(x);
 
-x += 10;
-console.log(x);
-x *= 10;
-console.log(x);
-x -= 10;
-console.log(x);
-x /= 10;
-console.log(x);
-x++;
-console.log(x);
-x--;
-console.log(x);
+// x += 10;
+// console.log(x);
+// x *= 10;
+// console.log(x);
+// x -= 10;
+// console.log(x);
+// x /= 10;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
 
 // ! Comparison Operator
-console.log(ageShahadat > agePinky);
-console.log(agePinky >= 18);
+// console.log(ageShahadat > agePinky);
+// console.log(agePinky >= 18);
 
-const isFullAge = agePinky >= 18;
+// const isFullAge = agePinky >= 18;
 
-console.log(currentYear - 1991 > currentYear - 2019);
+// console.log(currentYear - 1991 > currentYear - 2019);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const averageAge = (ageShahadat + agePinky) / 2;
+// console.log(ageShahadat, agePinky, averageAge)
+
+//  ! Coding Challenge #1
+
+// const massOfMark = 78;
+// const heightOfMark = 1.69;
+// const massOfJohn = 92;
+// const heightOfJohn = 1.95;
+
+// const massOfMark = 95;
+// const heightOfMark = 1.88;
+// const massOfJohn = 85;
+// const heightOfJohn = 1.76;
+// const markBMI = massOfMark / (heightOfMark * heightOfMark);
+// const johnBMI = massOfJohn / (heightOfJohn * heightOfJohn);
+
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI, johnBMI, markHigherBMI);
+
+// ! String
+
+// const firstName = "Shahadat";
+// const job = "programmer";
+// const birthYear = 2001;
+// const currentYear = 2023;
+
+// const bioOfShahadat = `I am ${firstName}, a ${
+//   currentYear - birthYear
+// } years old ${job}!`;
+
+// console.log(bioOfShahadat);
+
+// console.log(`Just a regular string...`);
+// console.log(
+//   "string with\n\
+// multiple \n\
+// lines"
+// );
+// console.log(`hi
+// how are u?
+// bro!`);
+
+// ! If else
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("You can have your license! 🚗");
+// } else {
+//   console.log(`you have to wait ${18 - age} years to have your license!`);
+// }
+
+// const birthYear = 1991;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
