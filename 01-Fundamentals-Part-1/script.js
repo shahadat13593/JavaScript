21cdd3f28c8386d9efe54609d9b302446f11728b
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2023-01-02 16:44:45 +0600

    01

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -29,13 +29,46 @@
 
 //! let, const, var
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 2001;
+// const birthYear = 2001;
 // birthYear = 2000;
 // const job;
 
-var job = "teacher";
+// var job = "teacher";
 
-const 
+// ! Basic Operator
+
+// ! Arithmetic Operator
+const currentYear = 2023;
+const ageShahadat = currentYear - 2001;
+const agePinky = currentYear - 2002;
+console.log(ageShahadat, agePinky);
+
+// ? 2 ** 3 means 2 to the power 3
+console.log(ageShahadat * 2, ageShahadat / 10, 2 ** 3);
+
+const firstName = "Shahadat";
+const lastName = "Hossain";
+console.log(firstName + " " + lastName);
+
+// ! Assignment Operator
+let x = 10 + 5;
+console.log(x);
+
+x += 10;
+console.log(x);
+x *= 10;
+console.log(x);
+x -= 10;
+console.log(x);
+x /= 10;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+// ! Comparison Operator
+console.log(ageShahadat > agePinky);
