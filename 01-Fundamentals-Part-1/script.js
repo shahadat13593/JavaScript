d0bf20111135d38bb224f2a25b87e1e38ab4719e
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2023-01-02 15:53:32 +0600

    01

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,41 @@
+// let js = "yes";
+// console.log(30 + 90);
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Raiyan";
+// console.log(firstName);
+// let PI = 3.1416;
+// console.log(PI);
+
+// ! Data type
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof "shahadat");
+
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// year = 2001;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+//! let, const, var
+
+let age = 30;
+age = 31;
+
+const birthYear = 2001;
+// birthYear = 2000;
+// const job;
+
+var job = "teacher";
+
+const 
