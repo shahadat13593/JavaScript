e449efe0f27bc1a964a7b5c15b791b562a2c8ca2
shahadat13593 <shahadat15-13593@diu.edu.bd>
2023-01-03 18:49:27 +0600

    01

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -143,3 +143,79 @@
 //   century = 21;
 // }
 // console.log(century);
+
+// ! Coding Challenge #2
+
+// const massOfMark = 78;
+// const hightOfMark = 1.69;
+// const massOfJohn = 92;
+// const hightOfJohn = 1.95;
+
+// const massOfMark = 95;
+// const hightOfMark = 1.88;
+// const massOfJohn = 85;
+// const hightOfJohn = 1.76;
+
+// const BIMOfMark = massOfMark / (hightOfMark * hightOfMark);
+// const BIMOfJohn = massOfJohn / (hightOfJohn * hightOfJohn);
+
+// if (BIMOfMark > BIMOfJohn) {
+//   console.log(
+//     `Mark's BMI (${BIMOfMark}) is higher than John's (${BIMOfJohn})!`
+//   );
+// } else {
+//   console.log(
+//     `John's BMI (${BIMOfJohn}) is higher than Mark's (${BIMOfMark})!`
+//   );
+// }
+
+// ! Type conversion and coercion
+
+// ! Type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("shahadat"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// ! Type coercion
+
+// console.log("I am " + 23 + " years old");
+// console.log("23" - 12);
+// console.log("2" * 3);
+// console.log("22" / 2);
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// ! Truthy and Falsy Values
+
+// ! Falsy Values : 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean("shahadat"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+
+// if (money) {
+//   console.log("Don't spend it all!");
+// } else {
+//   console.log("You should get a job");
+// }
+
+// let hight = 0;
+// if (hight) {
+//   console.log(`YaY! Hight is defined!`);
+// } else {
+//   console.log("Hight is not defined!");
+// }
+
+//! Equality Operator == vs. ===
