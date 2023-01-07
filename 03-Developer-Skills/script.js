a87f90b80926668e2f14cd2531b279f722dd768b
shahadat13593 <shahadat15-13593@diu.edu.bd>
2023-01-08 01:19:27 +0600

    05

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -54,3 +54,66 @@
 // };
 
 // calcTempAmplitude([1, 2, 3, -9], temperature);
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
+//     value: Number(prompt("Degree Celsius:")),
+//   };
+
+//   // console.log(measurement.value);
+//   //   console.log(measurement);
+//   console.table(measurement);
+//   //   console.warn(measurement.value);
+//   //   console.error(measurement.value);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+// ! Bug
+
+// const calcTempAmplitudeBug = function (temp1, temp2) {
+//   const temps = temp1.concat(temp2);
+//   let min = temps[0];
+//   let max = temps[0];
+//   for (let i = 0; i <= temps.length - 1; i++) {
+//     // debugger;
+//     if (typeof temps[i] !== "number") continue;
+//     if (temps[i] < min) min = temps[i];
+
+//     if (temps[i] > max) max = temps[i];
+//   }
+//   console.log(max, min);
+//   const tempAmplitudeBug = max - min;
+//   return console.log(tempAmplitudeBug);
+// };
+
+// calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+//! Coding Challenge #1
+
+// ! 1) Understanding  the problems
+// What is forecasted? :to calculate or predict usually as a result of study and analysis of available pertinent data
+
+//!Breaking up into sub-problems
+
+// create a function printForecast
+// have to pass arr as argument
+// have to use a loop in array
+// return each items with it's days
+
+// const printForecast = function (arr) {
+//   let string = "";
+
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     string += `${arr[i]}C in ${i + 1}...`;
+//   }
+//   console.log("... " + string);
+// };
+
+// printForecast([17, 21, 23]);
+// printForecast([12, 5, -5, 0, 4]);
